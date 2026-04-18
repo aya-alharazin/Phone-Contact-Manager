02e478dd13ca61108234f023c2d520f1aae04866
ayoosh <alharazinaya@gmail.com>
2026-04-18 18:25:48 +0200

    first commit

---
+++ app/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Main to edit this template
+ */
+package app;
+
+/**
+ *
+ * @author aya
+ */
+public class Main {
+    
+}
