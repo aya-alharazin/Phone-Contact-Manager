f92370d9c41e5dfd77c80a4606e3b3a0acd6da14
ayoosh <alharazinaya@gmail.com>
2026-04-19 12:30:30 +0200

    Clean up Main.java by adding whitespace for improved readability

--- app/Main.java
+++ app/Main.java
@@ -18,7 +18,28 @@ import javafx.stage.Stage;
 public class Main extends Application{
     public static void main(String [] args){
         
-        launch(args);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 
